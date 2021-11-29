c7908c9bcf0da9bb959a9b893125eed11c8ebd9f
Weesus <70699787+Weesuus@users.noreply.github.com>
2021-11-29 19:42:19 +0100

    Update helloWorld.py

--- Python/one/helloWorld.py
+++ Python/one/helloWorld.py
@@ -1,1 +1,5 @@
 print('Hello World!')
+
+# Grazie al Cancelletto possiamo scrivere dei Commenti alle Istruzioni
+# Per delimitare le Stringhe possiamo usare sia '' che ""
+# Quindi diventerebbe print("Hello World!")
